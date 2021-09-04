55b32f1145023254369e471258083f9fb4b28df4
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-09-04 19:12:17 +0530

    installed antd and style components

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NavBar } from "./style";
+
+export default function Header() {
+    return (
+        <NavBar>
+            
+        </NavBar>
+    )
+}
---
+++ client/src/components/Header/style.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+
+export const NavBar = styled.div`
+    min-height:20px;
+`;
