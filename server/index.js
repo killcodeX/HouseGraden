6c93706d57b99b16d5501de22f426ddd3260ae39
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-09-04 18:25:21 +0530

    setup some file in index js

---
+++ server/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import hotelRoutes from "./routes/route.js";
+import dotenv from "dotenv";
+import Razorpay from "razorpay";
+import shortid  from "shortid";
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// for setting up razorpay
+// export const razorpay = new Razorpay({
+//   key_id: process.env.RAZOR_KEY_ID,
+//   key_secret: process.env.RAZOR_KEY_SECRET,
+// });
+
+// sholud be called after initializing cors to avoid cors origin issue
+app.use("/hotelin", hotelRoutes);
+
+// for database
+const connectionUrl = `mongodb+srv://Aaquib5076:${process.env.DB_PASSWORD}@cluster0.oknup.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(connectionUrl, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port : ${PORT}`))
+  )
+  .catch((err) => console.log(err));
+
+mongoose.set("useFindAndModify", false);
