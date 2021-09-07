851881bff72a6b0cd99bd31a32089042d2c53be2
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-09-07 16:58:22 +0530

    config axios and setup fetch all product api

---
+++ client/src/api/postApi.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const baseURL = process.env.NODE_ENV ==='development'? "http://localhost:5000": ""
+
+export const ApiFunc = axios.create({ baseURL: baseURL });
+
+ApiFunc.interceptors.request.use((req) => {
+  if (localStorage.getItem("housegardentoken")) {
+    req.headers.Authorization = `Bearer ${JSON.parse(
+      localStorage.getItem("housegardentoken")
+    )}`;
+  }
+  return req;
+});
+
+
+// User Api
+export const getAllProductApi = async () => {
+    try {
+      const { data } = await ApiFunc.get(`/housegarden/get-all-products`);
+      return data;
+    } catch (error) {
+      if (error.response) {
+        openNotificationWithIcon(
+          "error",
+          "Fetching All Products Failed",
+          error.response.data.message
+        );
+      }
+    }
+  };
