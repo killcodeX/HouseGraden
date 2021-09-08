1d7833fa577e71915f10d7165b1163f9c7110528
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-09-08 16:05:35 +0530

    created new pages and routes

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,10 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./container/Home";
 import SingleProduct from "./container/SingleProduct";
+import Cart from "./container/Cart";
+import Checkout from "./container/Checkout";
+import Orders from "./container/Orders";
+import WishList from "./container/WishList";
 
 function App() {
   const { width } = useWindowSize();
@@ -21,6 +25,10 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/product/:id" component={SingleProduct} />
+        <Route exact path="/my-cart" component={Cart} />
+        <Route exact path="/checkout" component={Checkout} />
+        <Route exact path="/my-orders" component={Orders} />
+        <Route exact path="/my-wishlist" component={WishList} />
       </Switch>
       <Footer/>
     </main>
---
+++ client/src/container/WishList/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function WishList() {
+    return (
+        <div>
+            This is WishList page
+        </div>
+    )
+}
