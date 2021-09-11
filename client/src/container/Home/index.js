dd7695b75e6a33d050ee063b530439a523b0f98c
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-09-11 16:43:21 +0530

    set home to render for diff cond

--- client/src/container/Home/index.js
+++ client/src/container/Home/index.js
@@ -4,15 +4,17 @@ import { HomeWrapper, SideMenuWrapper, NoDataBanner } from "./style";
 import SideMenus from "../../components/SideBar";
 import Searchform from "./searchform";
 import ProductCard from "../../components/ProductCard";
-import { useDispatch, useSelector, useselector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getAllProduct } from "../../redux/actions/postactions";
 
 export default function Home() {
   const { width } = useWindowSize();
   const dispatch = useDispatch();
   const sort = useSelector((state) => state.products.sortP);
+  const search = useSelector((state) => state.products.searchP);
   const products = useSelector((state) => state.products.allProducts);
   const sortedProduct = useSelector((state) => state.products.sortedProduct);
+  const searchedProduct = useSelector((state) => state.products.searchProduct);
 
   useEffect(() => {
     dispatch(getAllProduct());
@@ -40,6 +42,50 @@ export default function Home() {
     );
   }
 
+  if (sort) {
+    return (
+      <HomeWrapper>
+        {width > 1000 ? (
+          <SideMenuWrapper>
+            <SideMenus />
+          </SideMenuWrapper>
+        ) : null}
+        <Searchform />
+        <div className="row mt-5">
+          {sortedProduct.map((item) => {
+            return (
+              <div key={item._id} className="col-sm-4 mb-5">
+                <ProductCard item={item} />
+              </div>
+            );
+          })}
+        </div>
+      </HomeWrapper>
+    );
+  }
+
+  if (search) {
+    return (
+      <HomeWrapper>
+        {width > 1000 ? (
+          <SideMenuWrapper>
+            <SideMenus />
+          </SideMenuWrapper>
+        ) : null}
+        <Searchform />
+        <div className="row mt-5">
+          {searchedProduct.map((item) => {
+            return (
+              <div key={item._id} className="col-sm-4 mb-5">
+                <ProductCard item={item} />
+              </div>
+            );
+          })}
+        </div>
+      </HomeWrapper>
+    );
+  }
+
   return (
     <HomeWrapper>
       {width > 1000 ? (
@@ -49,21 +95,13 @@ export default function Home() {
       ) : null}
       <Searchform />
       <div className="row mt-5">
-        {sort
-          ? sortedProduct.map((item) => {
-              return (
-                <div key={item._id} className="col-sm-4 mb-5">
-                  <ProductCard item={item} />
-                </div>
-              );
-            })
-          : products.map((item) => {
-              return (
-                <div key={item._id} className="col-sm-4 mb-5">
-                  <ProductCard item={item} />
-                </div>
-              );
-            })}
+        {products.map((item) => {
+          return (
+            <div key={item._id} className="col-sm-4 mb-5">
+              <ProductCard item={item} />
+            </div>
+          );
+        })}
       </div>
     </HomeWrapper>
   );
