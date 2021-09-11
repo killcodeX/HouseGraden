7c15f94ff9c9d5bb7e3b09ba579fdae2be248532
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-09-11 22:14:08 +0530

    created login and register api function

---
+++ client/src/api/userApi.js
@@ -0,0 +1,42 @@
+import { ApiFunc } from "./postApi";
+
+const openNotificationWithIcon = (type, title, message) => {
+    notification[type]({
+      message: title,
+      description: message,
+    });
+  };
+
+
+// User Api
+export const userRegister = async (body) => {
+  try {
+    const { data } = await ApiFunc.post(`/housegarden/signup`, body);
+    openNotificationWithIcon("success", "Signup Successful", "");
+    return data;
+  } catch (error) {
+    if (error.response) {
+      openNotificationWithIcon(
+        "error",
+        "Signup Failed",
+        error.response.data.message
+      );
+    }
+  }
+};
+
+export const userLogin = async (body) => {
+  try {
+    const { data } = await ApiFunc.post(`/housegarden/signin`, body);
+    openNotificationWithIcon("success", "Login Successful", "");
+    return data;
+  } catch (error) {
+    if (error.response) {
+      openNotificationWithIcon(
+        "error",
+        "Login Failed",
+        error.response.data.message
+      );
+    }
+  }
+};
