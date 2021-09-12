9c9564ed0ef92927140ebfb2a3eec8ffe4fea86e
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-09-12 16:41:14 +0530

    removed unnescaary file

--- client/src/components/ProductCard/index.js
+++ client/src/components/ProductCard/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { AiFillStar, AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import {
