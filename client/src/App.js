2aef5651aefda8b1c5528d4b1b23946fcc758834
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-09-12 11:39:03 +0530

    added verify storage actions

--- client/src/App.js
+++ client/src/App.js
@@ -17,10 +17,20 @@ import Orders from "./container/Orders";
 import WishList from "./container/WishList";
 import Login from "./container/Auth/login";
 import Register from "./container/Auth/register";
+import { useDispatch, useSelector } from "react-redux";
+import { verifyStorage } from "./redux/actions/authactions";
 
 function App() {
   const { width } = useWindowSize();
   const location = useLocation();
+  const dispatch = useDispatch()
+  const auth = useSelector((state) => state.auth.isAuthenticated);
+
+  useEffect(() => {
+    dispatch(verifyStorage());
+  }, [auth]);
+
+
   return (
     <main className='position-relative'>
       {location.pathname !== "/login" && location.pathname !== "/register" ? (
