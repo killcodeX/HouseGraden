51945c1ca2f4600f65571f8df39ab3deb33b87c1
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-09-13 19:12:07 +0530

    created order modal

---
+++ server/models/orderModel.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const { ObjectId } = mongoose.Schema.Types;
+
+const postSchema = new mongoose.Schema({
+  productId: [{ type: ObjectId, ref: "ProductMessage" }],
+  address: {
+    fname: { type: String, required: true },
+    lname: { type: String, required: true },
+    mobile: { type: String, required: true },
+    houseno: { type: String, required: true },
+    street: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    pincode: { type: String, required: true },
+  },
+  totalAmount: { type: String, required: true },
+  orderId: { type: String, required: true },
+  userId: { type: ObjectId, ref: "UserMessage" },
+  status: { type: String, required: true },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+  updatedAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+const OrderMessage = mongoose.model("OrderMessage", postSchema);
+
+export default OrderMessage;
